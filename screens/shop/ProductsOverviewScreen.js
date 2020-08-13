071955ac5f52154122ba590c01b7609f0370ed41
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-12 22:43:21 -0700

    Created ProductsOverviewScreen template

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+
+const ProductsOverviewScreen = (props) => {
+    return <FlatList />;
+};
+
+export default ProductsOverviewScreen;
+
+const styles = StyleSheet.create({});

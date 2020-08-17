5efc1f05d61b617b33223ff78b8392fc004c0505
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-16 17:35:31 -0700

    added DELETE_PRODUCT reducer

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,6 +1,7 @@
 import {ADD_TO_CART, REMOVE_FROM_CART} from '../actions/cart';
 import CartItem from '../../models/cart-item';
 import {ADD_ORDER} from '../actions/orders';
+import {DELETE_PRODUCT} from '../actions/products';
 
 const initialState = {
     items: {},
@@ -72,6 +73,20 @@ export default (state = initialState, action) => {
             };
         case ADD_ORDER:
             return initialState;
+        case DELETE_PRODUCT:
+            if (!state.items[action.pid]) {
+                // check if item is in the cart
+                return state; // if not in the cart, it will return the existing state
+            }
+
+            const updatedItems = {...state.items}; // copy all items in state
+            const itemTotal = state.items[action.pid].sum; // get the item total
+            delete updatedItems[action.pid]; // delete that item from the state (the cart)
+            return {
+                ...state,
+                items: updatedItems,
+                totalAmount: state.totalAmount - itemTotal,
+            };
     }
     return state;
 };

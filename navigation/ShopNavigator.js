218c0cf200eca123f0c9b0dc36e6a586cf5ded52
Kingsley Ramos <Kingsleyramos@gmail.com>
2020-08-19 23:41:19 -0700

    Created custom logout component in drawer

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import {createAppContainer} from 'react-navigation';
-import {Platform} from 'react-native';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {Platform, SafeAreaView, Button, View} from 'react-native';
 import {createStackNavigator} from 'react-navigation-stack';
-import {createDrawerNavigator} from 'react-navigation-drawer';
+import {
+    createDrawerNavigator,
+    DrawerNavigatorItems,
+} from 'react-navigation-drawer';
 import {Ionicons} from '@expo/vector-icons';
+import {useDispatch} from 'react-redux';
 
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
@@ -11,7 +15,10 @@ import CartScreen from '../screens/shop/CartScreen';
 import OrdersScreen from '../screens/shop/OrdersScreen';
 import UserProductsScreen from '../screens/user/UserProductsScreen';
 import EditProductScreen from '../screens/user/EditProductScreen';
+import AuthScreen from '../screens/user/AuthScreen';
+import StartupScreen from '../screens/StartupScreen';
 import Colors from '../constants/Colors';
+import * as authActions from '../store/actions/auth';
 
 const defaultNavOptions = {
     headerStyle: {
@@ -98,7 +105,45 @@ const ShopNavigator = createDrawerNavigator(
         contentOptions: {
             activeTintColor: Colors.primary,
         },
+        contentComponent: (props) => {
+            const dispatch = useDispatch();
+            return (
+                <View style={{flex: 1, paddingTop: 20}}>
+                    <SafeAreaView
+                        forceInset={{top: 'always', horizontal: 'never'}}
+                    >
+                        {/* pass all the current drawer items */}
+                        <DrawerNavigatorItems {...props} />
+                        <Button
+                            title='Logout'
+                            color={Colors.primary}
+                            onPress={() => {
+                                dispatch(authActions.logout());
+                                //props.navigation.navigate('Auth');
+                                // Since we clear the token in redux, this will navigate to auth screen
+                                // check navigation/NavigationContainer.js
+                            }}
+                        />
+                    </SafeAreaView>
+                </View>
+            );
+        },
     }
 );
 
-export default createAppContainer(ShopNavigator);
+const AuthNavigator = createStackNavigator(
+    {
+        Auth: AuthScreen,
+    },
+    {
+        defaultNavigationOptions: defaultNavOptions,
+    }
+);
+
+const MainNavigator = createSwitchNavigator({
+    Startup: StartupScreen, // this is the first thing that loads
+    Auth: AuthNavigator,
+    Shop: ShopNavigator,
+});
+
+export default createAppContainer(MainNavigator);
